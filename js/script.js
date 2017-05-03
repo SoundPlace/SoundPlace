5a75448e3b06995533890033aa38107fa85db35c
Alexander <kinonadzami98@gmail.com>
2017-05-03 20:26:15 +0300

    Signed-off-by: Alexander <kinonadzami98@gmail.com> Modified   .DS_Store Modified   css/.DS_Store Modified   css/style.css Modified   img/.DS_Store Modified   img/verified-members-item-1.jpg Modified   img/verified-members-item-3.jpg Modified   img/verified-members-item-5.jpg Modified   img/verified-members-item-6.jpg Modified   index.html Modified   js/.DS_Store Modified   player/.DS_Store Modified   player/mediaelementplayer.css Add        Users.sql Add        img/inner-bg.jpg Add        img/verified-members-item-2.jpg Add        img/verified-members-item-4.jpg Add        js/back-to-top.js Add        js/script.js Add        login.html Add        login.php Add        profile.html Add        signup-2.html Add        signup.html

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function(){
+  $('.header-carousel.slick-slider').slick({
+	   	slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 10000,
+		infinite: true,
+	  	speed: 500,
+	  	fade: true,
+	  	cssEase: 'linear'
+  	});
+
+  $('.testimonials-carousel.slick-slider').slick({
+	   	slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 10000,
+		draggable: false,
+		pauseOnFocus: true,
+
+	});	
+
+  $(window).bind('scroll',function(){
+  	if($(window).scrollTop()>0){
+  		$('.logo-nav-box').addClass('navbar-fixed-top')}
+	else{
+		$('.logo-nav-box').removeClass('navbar-fixed-top')}
+	});
+
+  $('audio').mediaelementplayer();
+
+});
+
+
